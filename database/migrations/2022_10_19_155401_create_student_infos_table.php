ad9fe50d5177fc2cb0307be3bf549b2501d9f6c0
Sazid <sazidfahim49@gmail.com>
2022-10-21 12:07:10 +0600

    Project Install

---
+++ database/migrations/2022_10_19_155401_create_student_infos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_infos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('student_id');
+            $table->string('address');
+            $table->string('student_area');
+            $table->string('college');
+            $table->string('mobile');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student_infos');
+    }
+};
